3d8f650088f1f0fd8479cbcfc7472d009c9587e9
musimujanya <salsabilaa@ristek.cs.ui.ac.id>
2025-07-01 12:21:43 +0700

    integrate minigame

--- Assets/Scripts/Minigame/PuzzleManager.cs
+++ Assets/Scripts/Minigame/PuzzleManager.cs
@@ -5,6 +5,11 @@ public class PuzzleManager : MonoBehaviour
     public TargetSlot[] slots;
     public GameObject successPanel;
 
+    public SceneDialogueTrigger dialogueTrigger; 
+    public SceneTransitionAfterDialogue sceneTransition; 
+
+    private bool waitingForContinue = false;
+
     public void CheckWin()
     {
         foreach (TargetSlot slot in slots)
@@ -17,9 +22,26 @@ public class PuzzleManager : MonoBehaviour
 
         ShowSuccessUI();
     }
-    
+
     void ShowSuccessUI()
     {
         successPanel.SetActive(true);
+        waitingForContinue = true;
+    }
+
+    void Update()
+    {
+        if (waitingForContinue && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("dialog");
+
+            successPanel.SetActive(false);
+
+            dialogueTrigger.autoStart = true; 
+
+            sceneTransition.enabled = true; 
+
+            waitingForContinue = false;
+        }
     }
 }
