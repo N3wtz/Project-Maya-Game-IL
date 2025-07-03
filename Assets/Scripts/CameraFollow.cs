5decf871816be52f5a962a25ad4a0031e08c60ee
fiqhi11 <ismiransa@gmail.com>
2025-07-03 10:48:31 +0700

    adjustment & re layouting
    camera scripts
    player object
    obstacle object

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,15 @@ public class CameraFollow : MonoBehaviour
     public float FollowSpeed = 3f;
     public Transform target;
     public float xOffset = 0f;
-    public float yOffset = 2.4f;
+
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
+        // Gunakan posisi Y dan Z kamera saat ini
+        float fixedY = transform.position.y;
+        float fixedZ = transform.position.z;
+
+        // Kamera hanya mengikuti X
+        Vector3 newPos = new Vector3(target.position.x + xOffset, fixedY, fixedZ);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
